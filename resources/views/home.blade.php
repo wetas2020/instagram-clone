867c2988ad4c63d62234923a50da31aca7f0d43f
wetas2020 <62056784+wetas2020@users.noreply.github.com>
2021-11-25 23:26:38 +0100

    Designing the UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle " src="/svg/profile_image.jpg" alt="profile_image">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>wetaaaaas</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>0</strong> posts</div>
+                <div class="pr-5"><strong>207</strong> followers</div>
+                <div class="pr-5"><strong>1,248</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Saad Wetas</div>
+            <div>
+                Software engineer <br>
+                Rabat 🇲🇦 <br>
+                - The best things come from living outside of your comfort zone.
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="unsplash image">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://images.unsplash.com/photo-1633114072460-c7dd0b7c6161?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="unsplash image">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="unsplash image">
         </div>
     </div>
 </div>
