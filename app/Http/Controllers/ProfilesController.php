9453eb44b177bd844653f69a4e15b7721927ffc5
wetas2020 <62056784+wetas2020@users.noreply.github.com>
2021-11-30 18:40:26 +0100

    Make Use of Cache for Expensive Query

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -13,11 +14,27 @@ class ProfilesController extends Controller
         // $user = User::findOrFail($user);
         $follows = auth()->user() ? auth()->user()->following->contains($user->id) : false;
 
+        $postCount = Cache::remember('count.posts' . $user->id, now()->addSecond(30), function () use ($user){
+            return $user->posts->count();
+        });
+
+        $followersCount = Cache::remember('count.followers' . $user->id, now()->addSecond(30), function () use ($user){
+            return $user->profile->followers->count();
+        });
+
+        $followingCount = Cache::remember('count.following' . $user->id, now()->addSecond(30), function () use ($user){
+            return $user->following->count();
+        });
+
+
         // dd($follows);
 
         return view('profiles.index', [
             'user' => $user,
             'follows' => $follows,
+            'postCount' => $postCount,
+            'followersCount' => $followersCount,
+            'followingCount' => $followingCount,
         ], );
     }
 
